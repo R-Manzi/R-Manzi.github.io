059dbf4aad1cb8e33904bd0816f44d8266c66f71
R-Manzi <111302767+R-Manzi@users.noreply.github.com>
2022-10-14 18:36:13 -0400

    effacer js files copier js

--- script.js
+++ script.js
@@ -1,33 +1,125 @@
-const FIGURES = ['♦','♣','♥','♠']
-const VALEURS = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+/*document.querySelector("#reset").addEventListener("click",reset)**/
+document.querySelector("#shuffle").addEventListener("click",shuffle)
+
+const SUITS = ['♦','♣','♥','♠']
+const VALUES = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
 
 
+class Paquet {
+    constructor(cards = nouveauPaquet()) {
+    this.cards = cards 
+    }
 
-function creationPaquet(){
-    let cards = [];
-    for( var count_figure = 0; count_figure < this.FIGURES.length; count_figure++ ) {
-        for( var count_valeur = 0; count_valeur < this.VALEURS.length; count_valeur++ ) {
-            cards.push( new Carte( count_valeur+1, this.VALEURS[count_valeur], this.FIGURES[count_figure] ) );
+
+    get fullCartes() {
+        return this.cards.length
+
+    }
+
+    get halfCartes () {
+        return this.cards.length /2
+
+    }
+
+    
+    shuffle() {
+        const newPaquet = []
+        const newHalf1 = this.cards.slice(0,this.fullCartes-26);
+        const newHalf2 = this.cards.slice(this.halfCartes,this.fullCartes);
+        let i = 0;
+        while (i < this.halfCartes){
+            newPaquet.push(newHalf1[i]);
+            newPaquet.push(newHalf2[i]);
+            i++;
+
         }
-    }
-    return cartes;
-{
 
-function separer (sep_deck, div){
-    for (let index = 0; index < sep_deck.length; index++){
-        const division = sep_deck[index]
-        document.getElementById(identifiant)
+
+        this.cards = newPaquet
+        return newPaquet;
+        
     }
+
+
+
+
+    }
+    assamblage () {
+        this.cards = []
+        for (let suit in SUITS){
+            for (let value in VALUES) {
+                this.cards.push(`${VALUES[value]}${SUITS[suit]}`)
+            }
+        }
+        let assamblage = document.getElementById('decks');
+        assamblage.innerHTML = "";
+        let i = 0;
+        let j = 0;
+        while (i < 4){
+            assamblage.innerHTML = "div>"
+            i++;
+            while (j < 13){
+                assamblage.innerHTML += '<img scr = "' + images[13*i+j] + '" />';
+                j++;
+            assamblage.innerHTML = "</div>";}}
+
+            }
+        }
+
+    
+
+
+    }
+  
+
+class Carte {
+    constructor(figure, value) {
+        this.figure = figure
+        this.value = value  
+    }
+
+
+
+
+
+
+}
+
+function nouveauPaquet() { 
+    return SUITS.flatMap(figure => {
+        return VALUES.map(valeur => {
+            return new Carte (figure,valeur)
+        })
+    })
+
+
+
+
+}
 
 
     
 
-console.log(cards);
 
-let resultat = document.querySelector('#carte')
-resultat.innerHTML = (cards)
-document.querySelector('#brasser').addEventListener('click',cards)
+const deck = new Paquet()
+deck.shuffle()
+deck.assamblage()
+
+console.log(deck.cards)
+
+
+/*
+cardDiv = document.createElement("div")
+cardDiv.innerText = this.suit
+cardDiv.classList.add("card", this.color)
+cardDiv.dataset.value = `${this.value} ${this.suit}`
+*/
 
 
 
-const pageTittle = document.querySelector("h2")
+
+
+
+
+
+
