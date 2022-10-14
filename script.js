c79a06b30c35db9532b864ceed912f25839cc1df
R-Manzi <111302767+R-Manzi@users.noreply.github.com>
2022-10-14 18:25:29 -0400

    retrouver

---
+++ script.js
@@ -0,0 +1,33 @@
+const FIGURES = ['♦','♣','♥','♠']
+const VALEURS = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+
+
+
+function creationPaquet(){
+    let cards = [];
+    for( var count_figure = 0; count_figure < this.FIGURES.length; count_figure++ ) {
+        for( var count_valeur = 0; count_valeur < this.VALEURS.length; count_valeur++ ) {
+            cards.push( new Carte( count_valeur+1, this.VALEURS[count_valeur], this.FIGURES[count_figure] ) );
+        }
+    }
+    return cartes;
+{
+
+function separer (sep_deck, div){
+    for (let index = 0; index < sep_deck.length; index++){
+        const division = sep_deck[index]
+        document.getElementById(identifiant)
+    }
+
+
+    
+
+console.log(cards);
+
+let resultat = document.querySelector('#carte')
+resultat.innerHTML = (cards)
+document.querySelector('#brasser').addEventListener('click',cards)
+
+
+
+const pageTittle = document.querySelector("h2")
